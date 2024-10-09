f440996ca22fa8e0295314f08a9b93539ff39d57
Alexandra Hriscu <ah2148@kent.ac.uk>
2024-10-09 12:02:34 +0000

    completion

--- partSix.py
+++ partSix.py
@@ -1,1 +1,11 @@
+student = input("Are you a student?: ").lower()
+age = int(input("Enter age: "))
 
+if age <12 or age >=65:
+    price = 5
+elif student == "yes":
+    price = 8
+else:
+    price = 10
+
+print(f"Your ticket is £{price}")
