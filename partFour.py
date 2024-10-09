d6a144ce766da593b85635bdc02a129cfcf5dc4e
Alexandra Hriscu <ah2148@kent.ac.uk>
2024-10-09 12:04:25 +0000

    completion

--- partFour.py
+++ partFour.py
@@ -1,1 +1,8 @@
+username = input("What is your username: ")
+password = input("What is your password: ")
+
+if username=="admin" and password=="password123":
+    print("Username and password correct")
+else:
+    print("Username or password incorrect")
 
