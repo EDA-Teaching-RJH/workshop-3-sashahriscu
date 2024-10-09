9c0dbd22bf6570d7292de618032271b6e96fefe4
Alexandra Hriscu <ah2148@kent.ac.uk>
2024-10-09 12:47:19 +0000

    completion / ?

--- partSeven.py
+++ partSeven.py
@@ -1,1 +1,38 @@
+def main():
+    num1 = int(input("Input first number: "))
+    num2 = int(input("Input second number: "))
+    operation = input("Pick operation: (+, -, /, *, ^, or %), type 'quit' to cancel operation. ")
+    
+    if operation.lower()=="quit":
+        print("Cancelling operation.")
+    break # ??????? outside loop ????????
 
+#??????????????
+    match case:
+            case "+":
+                result = (num1 + num2)
+                print(f"{num1}+{num2} = {result}")
+            case "-":
+                result = (num1 - num2)
+                print(f"{num1}-{num2} = {result}")
+            case "/":
+                if num2==0:
+                    return("Error! Cannot divide by 0.")
+                else:
+                    result = (num1 / num2)
+                    print(f"{num1}/{num2} = {result}")
+            case "*":
+                result = (num1 * num2)
+                print(f"{num1}*{num2} = {result}")
+            case "^":
+                result = (num1 ^ num2)
+                print(f"{num1} ^ {num2} = {result}")
+            case "%":
+                if num2==0:
+                    print ("Error! Cannot perform modulo by 0.")
+                else:
+                    result = (num1 % num2)
+                    print(f"{num1} % {num2} = {result}")
+                    
+main()
+            
